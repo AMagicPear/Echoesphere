da9138312b2ae25061a4c106fc3b7903ec0cac34
AMagicPear <AMagicPear@outlook.com>
2026-01-27 16:30:53 +0800

    style: 添加.editorconfig文件配置C#左括号不换行

--- Assets/Scripts/Puzzle/AvatarRotating.cs
+++ Assets/Scripts/Puzzle/AvatarRotating.cs
@@ -3,25 +3,20 @@ using Stuff;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Puzzle
-{
-    public class AvatarRotating : MonoBehaviour
-    {
+namespace Puzzle {
+    public class AvatarRotating : MonoBehaviour {
         public RotatableAvatar[] avatars;
         public int[] rotationGroups;
 
-        public void RotateGroup(int groupIndex, bool forward)
-        {
+        public void RotateGroup(int groupIndex, bool forward) {
             int stride = forward ? 1 : -1;
-            foreach (var rotationGroup in rotationGroups)
-            {
+            foreach (var rotationGroup in rotationGroups) {
                 avatars[rotationGroup].RotateForward(stride);
             }
         }
 
         // 测试旋转
-        public void OnMove(InputValue value)
-        {
+        public void OnMove(InputValue value) {
             Console.WriteLine($"OnMove: {value}");
             // RotateGroup(0, value.Get<float>() > 0);
         }
--- Assets/Scripts/Stuff/RotatableAvatar.cs
+++ Assets/Scripts/Stuff/RotatableAvatar.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Collections;
-using UnityEngine;
-using UnityEngine.InputSystem;
+﻿using UnityEngine;
 
-namespace Stuff
-{
-    public class RotatableAvatar : MonoBehaviour
-    {
+namespace Stuff {
+    public class RotatableAvatar : MonoBehaviour {
         private int facingAt = 0;
-        public int FacingAt
-        {
+
+        public int FacingAt {
             get => facingAt;
-            private set
-            {
+            private set {
                 facingAt = value;
                 transform.rotation = Quaternion.Euler(0, 0, facingAt * 90);
             }
         }
+
         public int directionsCount = 4;
 
-        public int RotateForward(int stride = 1)
-        {
+        public int RotateForward(int stride = 1) {
             FacingAt = (FacingAt + stride + directionsCount) % directionsCount;
             return FacingAt;
         }
