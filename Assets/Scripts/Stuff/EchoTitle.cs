c451776017c9b8b74b7f55ebe1fabe4c50c812d8
AMagicPear <AMagicPear@outlook.com>
2026-04-20 23:22:53 +0800

    feat: 把文字搞进来 浮动特效

---
+++ Assets/Scripts/Stuff/EchoTitle.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+namespace Echoesphere.Runtime.Stuff {
+    public class EchoTitle : MonoBehaviour {
+        [Header("Echo Title")]
+        [Range(0f, 0.1f)] public float positionStrength = 0.005f;
+        [Range(0f, 10f)] public float positionSpeed = 1f;
+        [Range(0f, 5f)] public float rotationStrength = 0.5f;
+        [Range(0f, 10f)] public float rotationSpeed = 1f;
+
+        private Transform[] _children;
+        private Vector3[] _basePositions;
+        private Quaternion[] _baseRotations;
+        private Vector3[] _noiseOffsets;
+
+        private void Start() {
+            int childCount = transform.childCount;
+            _children = new Transform[childCount];
+            _basePositions = new Vector3[childCount];
+            _baseRotations = new Quaternion[childCount];
+            _noiseOffsets = new Vector3[childCount];
+
+            for (int i = 0; i < childCount; i++) {
+                _children[i] = transform.GetChild(i);
+                _basePositions[i] = _children[i].localPosition;
+                _baseRotations[i] = _children[i].localRotation;
+                _noiseOffsets[i] = new Vector3(
+                    Random.Range(0f, 100f),
+                    Random.Range(0f, 100f),
+                    Random.Range(0f, 100f)
+                );
+            }
+        }
+
+        private void Update() {
+            float t = Time.time;
+
+            for (int i = 0; i < _children.Length; i++) {
+                float px = Mathf.PerlinNoise(_noiseOffsets[i].x, t * positionSpeed) * 2f - 1f;
+                float py = Mathf.PerlinNoise(_noiseOffsets[i].y, t * positionSpeed + 10f) * 2f - 1f;
+                float pz = Mathf.PerlinNoise(_noiseOffsets[i].z, t * positionSpeed + 20f) * 2f - 1f;
+
+                _children[i].localPosition = _basePositions[i] + new Vector3(px, py, pz) * positionStrength;
+
+                float rx = Mathf.PerlinNoise(_noiseOffsets[i].x + 50f, t * rotationSpeed) * 2f - 1f;
+                float ry = Mathf.PerlinNoise(_noiseOffsets[i].y + 50f, t * rotationSpeed + 10f) * 2f - 1f;
+                float rz = Mathf.PerlinNoise(_noiseOffsets[i].z + 50f, t * rotationSpeed + 20f) * 2f - 1f;
+
+                _children[i].localRotation = _baseRotations[i] * Quaternion.Euler(rx * rotationStrength, ry * rotationStrength, rz * rotationStrength);
+            }
+        }
+    }
+}
--- Assets/Scripts/Stuff/Fog.cs
+++ Assets/Scripts/Stuff/Fog.cs
@@ -1,19 +1,31 @@
 using UnityEngine;
 
-namespace Echoesphere.Runtime.Stuff
-{
-    public class Fog : MonoBehaviour
-    {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
+namespace Echoesphere.Runtime.Stuff {
+    public class Fog : MonoBehaviour {
+        private static readonly int Offset = Shader.PropertyToID("_Offset");
+        [Header("Fog Offset")]
+        public Vector3 offsetSpeed = new(0.1f, 0f, 0.1f);
+        public Vector3 offsetWobble = new(0.02f, 0f, 0.02f);
+        public Material fogMaterial;
+
+        private Vector3 _currentOffset;
+
+        private void Start() {
+            _currentOffset = Vector3.zero;
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
+        private void Update() {
+            float wobbleX = Mathf.Sin(Time.time * 2f) * offsetWobble.x;
+            float wobbleZ = Mathf.Cos(Time.time * 1.7f) * offsetWobble.z;
+
+            _currentOffset.x += (offsetSpeed.x + wobbleX) * Time.deltaTime;
+            _currentOffset.z += (offsetSpeed.z + wobbleZ) * Time.deltaTime;
+
+            fogMaterial.SetVector(Offset, _currentOffset);
+        }
+
+        private void OnDestroy() {
+            fogMaterial.SetVector(Offset, Vector3.zero);
         }
     }
 }
